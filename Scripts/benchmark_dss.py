405ad6dccaa0aadbe7d6ee221bbb371835086a79
ocatias <fabian111@gmx.net>
2023-11-15 12:15:31 +0100

    update benchmark scripts

--- Scripts/benchmark_dss.py
+++ Scripts/benchmark_dss.py
@@ -6,24 +6,22 @@ import time
 
 import numpy as np
 from torch_geometric.data import Data
-import torch_geometric
-import torch
 import networkx as nx
 import matplotlib.pyplot as plt
-from torch_geometric.datasets import ZINC, GNNBenchmarkDataset
+from torch_geometric.datasets import ZINC
 from ogb.graphproppred import PygGraphPropPredDataset
-import pandas as pd
+from Models.ESAN.transform import policy2transform
 
 from Misc.config import config 
 from Misc.utils import edge_tensor_to_list
 from Misc.cyclic_adjacency_transform import CyclicAdjacencyTransform
 
 def main():
-    repeats = 10
+    repeats = 1
     results = []
-    for ds_name in ["zinc", "molhiv", "molbace", "molbbbp", "molsider", "moltoxcast", "molesol", "mollipo", "moltox21"]:
-        for use_spider_web in [False]:
-            print(f"\nRunning on {ds_name} \t Spiderweb: {use_spider_web}")
+    for ds_name in ["zinc"]:
+        for trafo in ["node_del", "edge_del"]:
+            print(f"\nRunning on {ds_name} \t trafo: {trafo}")
 
             if ds_name == "zinc":
                 ds = ZINC(root=config.DATA_PATH, subset=True, split="train")
@@ -32,7 +30,12 @@ def main():
                 split_idx = ds.get_idx_split()
                 ds = ds[split_idx["train"]]
             
-            transform = CyclicAdjacencyTransform(debug=True, spiderweb=use_spider_web)
+            if trafo == "ego":
+                transform = policy2transform("ego_nets", 3)
+            elif trafo == "edge_del":
+                transform = policy2transform("edge_deleted", 3)
+            else:
+                transform = policy2transform("node_deleted", 3)
             runtimes = []
                 
             # Add one iteration for warmup
@@ -49,7 +52,7 @@ def main():
             # Remove warmup iteration
             runtimes = runtimes[1:]
             print(f"{ds_name}\nRuntime: {np.mean(runtimes):.2f} +/- {np.std(runtimes):.2f}")
-            results.append(f"{ds_name} {'+Spider' if use_spider_web else ''}:{np.mean(runtimes):.2f} +/- {np.std(runtimes):.2f}")
+            results.append(f"{ds_name} {trafo}:{np.mean(runtimes):.2f} +/- {np.std(runtimes):.2f}")
             
     print("\n\n\n", "Results\n", "-"*20)
     print("\n\n".join(results))
