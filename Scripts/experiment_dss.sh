b0fe21b7e15bb59fb48948e631b2bed03db658b7
ocatias <fabian111@gmx.net>
2023-11-16 10:31:11 +0100

    Fix bug in DSS script

--- Scripts/experiment_dss.sh
+++ Scripts/experiment_dss.sh
@@ -9,7 +9,7 @@ python Exp/run_experiment.py -grid "Configs/DSS/$1_zinc.yaml" -dataset "ZINC" --
 for ds in "${datasets[@]}"
     do
     echo "$ds"
-    python Exp/run_experiment.py -grid "Configs/DSS/$1_ogb_small.sh" -dataset "$ds" --candidates 64  --repeats 5
+    python Exp/run_experiment.py -grid "Configs/DSS/$1_ogb_small.yaml" -dataset "$ds" --candidates 64  --repeats 5
     done
 
 
